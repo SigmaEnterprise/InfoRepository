b515be4ec016aeab90aabbb63260391a67ac030c
SigmaEnterprise <ceronode@tuta.io>
2025-08-13 16:57:05 -0400

    Switched Branch

--- config.rb
+++ config.rb
@@ -2,10 +2,11 @@ wiki_opts = {
   base_path: '/wiki',
   allow_uploads: 'dir',
   live_preview: true,
-  css: 'custom.css',       # Your custom CSS file
+  css: '.gollum/public/custom.css',  # Adjusted path
   show_all: true,
   universal_toc: true
 }
+
 
 Precious::App.set(:wiki_options, wiki_opts)
 
